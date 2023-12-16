177368ed38b670e083e3431a437dcabdb640c59f
TSUCHIDA Y <ChitsuoMan404@outlook.jp>
2023-12-16 12:33:01 +0900

    firstCommit

---
+++ Assets/Scripts/GridData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridData
+{
+    //Dictionary<Vector3Int, PlacementData> placedOjects = new();
+}
+
+//public class PlacementData
+//{
+//    public List<Vector3Int> occupiedPositions;
+
+//    public int ID {  get; private set; }
+
+//    public int PlacedObjectIndex { ge}
+//}
